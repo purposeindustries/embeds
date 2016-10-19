6ceef462d3436e2ba4a07ca5fa44e32deaeb2619
ellell <lisaovermyr@gmail.com>
2016-10-19 11:56:19 +0200

    Handle another type of giphy url

--- test/embed-code-parse-test.js
+++ test/embed-code-parse-test.js
@@ -54,6 +54,9 @@ test('giphy', t => {
   t.deepEqual(parseInput('https://giphy.com/embed/3oxRmeLK7bjcq0CCCA'), expected);
   t.deepEqual(parseInput('http://giphy.com/embed/3oxRmeLK7bjcq0CCCA'), expected);
   t.deepEqual(parseInput('//giphy.com/embed/3oxRmeLK7bjcq0CCCA'), expected);
+  t.deepEqual(parseInput('https://giphy.com/gifs/new-girl-fox-new-girl-newgirl-3oxRmeLK7bjcq0CCCA'), expected);
+  t.deepEqual(parseInput('http://giphy.com/gifs/new-girl-fox-new-girl-newgirl-3oxRmeLK7bjcq0CCCA'), expected);
+  t.deepEqual(parseInput('//giphy.com/gifs/new-girl-fox-new-girl-newgirl-3oxRmeLK7bjcq0CCCA'), expected);
   t.deepEqual(parseInput(code), expected);
 });
 
